7cf53c051ec6463ecbfaa42dec4b35ec4ca9e44f
Marcus <marcusmallum@gmail.com>
2018-02-26 15:29:43 -0600

    Simple socket.io client-server successful

---
+++ server.js
@@ -0,0 +1,21 @@
+//server.js
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function (socket){
+    console.log('connection');
+
+    socket.on('CH01', function (from, msg) {
+        console.log('MSG', from, ' saying ', msg);
+    });
+
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
